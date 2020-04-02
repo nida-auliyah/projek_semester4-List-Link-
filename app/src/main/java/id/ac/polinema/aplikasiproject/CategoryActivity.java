8be454f9b2b544c06b68ce49ea4c61676cc0f2e4
nida-auliyah <nidaauliyah@gmail.com>
2020-04-02 10:03:21 +0700

    progress pertama

--- app/src/main/java/id/ac/polinema/aplikasiproject/CategoryActivity.java
+++ app/src/main/java/id/ac/polinema/aplikasiproject/CategoryActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class CategoryActivity extends AppCompatActivity {
@@ -20,5 +22,10 @@ public class CategoryActivity extends AppCompatActivity {
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
 
+    }
+
+    public void handleFab(View view) {
+        Intent intent = new Intent(this, CategoryAddActivity.class);
+        startActivity(intent);
     }
 }
